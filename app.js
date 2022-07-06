2ed10cadedfe1dc35922ac41621b8112e6a09764
Vihanga Pramudith <88036867+VihangaPramudith@users.noreply.github.com>
2022-07-06 22:56:14 +0530

    implement app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+//products route
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connect to MongoDB
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+      console.log(`server started on port ; ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
